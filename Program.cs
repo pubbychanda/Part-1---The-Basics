0126e8f8894f723f589460c32eee172a4e3ae6df
Nolan Neilson <NeilNola532@GOTVDSB.CA>
2021-02-17 10:10:53 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace The_Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String str = @"N   N  N   N
+NN  N  NN  N            
+N N N  N N N                           
+N  NN  N  NN                           
+N   N. N   N.";                           
+            Console.WriteLine(str);
+
+            String firstName = "Nolan";
+            String favMovie = "The Matrix";
+            String greeting = "Hows it hangin' " + firstName + "? Have you watched your daily dose of " + favMovie + "?";
+
+            Console.WriteLine(greeting.ToLower());
+            Console.WriteLine(greeting.ToUpper());
+            Console.WriteLine("contains THE: " + greeting.ToUpper().Contains("THE"));
+            Console.WriteLine(greeting.ToUpper().Replace('A', '@').Replace('E', '3'));
+            Console.ReadLine();
+        }
+    }
+}
